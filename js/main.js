6f5115736f497ef3b16c14cf75fd7b19169cc84d
bastardicienta <latataalvarez1089@hotmail.com>
2021-10-18 19:11:54 -0500

    merge completo, con JS

--- js/main.js
+++ js/main.js
@@ -16,4 +16,23 @@ function filterMovies(){
     const moviesFound=movies.filter(movie=>movie.name===search)
     console.log(moviesFound)
 }
-filterMovies()
+
+function showMovies (){
+    let moviesList = document.getElementById("movies-list")
+    console.log(moviesList)
+    for (i=0; i< movies.length; i++){
+        console.log(movies[i])
+        const movieItem = '<div id="movies-list-movie-container" class="col">'+
+        '<div class="list-movie-img">'+
+            '<div class="list-stars-container">'+
+                '<i id="gray-star" class="fa-solid fa-star"></i>'+
+                '<i id="yellow-star" class="fa-solid fa-star"></i>'+
+                '<i id="yellow-star" class="fa-solid fa-star"></i>'+
+            '</div> <img class="photo" src="'+movies[i].image+'" alt="oxygen-movie">'+
+        '</div></div>'
+        moviesList.innerHTML+=movieItem
+    
+    }
+    console.log(moviesList)
+}
+showMovies()
